44cfb92efa068fe001cfef80ddc702a716fbc35d
Richard Kent Jordan <rjordan@pobox.com>
2010-11-18 22:55:00 -0600

    Initial Import

---
+++ SimpleStateMachine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleStateMachine {
+    public class State {
+        public State(string Key, string Description, string[] transitions) {
+            this.Key = Key;
+            this.Description = Description;
+            this.Transitions = transitions;
+        }
+
+        public string Key { get; private set; }
+        public string Description { get; private set; }
+        public string[] Transitions { get; private set; }
+
+        public delegate void EventHandler(string State);
+
+        public EventHandler OnEnter; //Pass previous state
+        public EventHandler OnExit; //Pass new state
+
+    }
+
+    [Serializable]
+    public abstract class StateMachine {
+        [NonSerialized]
+        private Dictionary<string, State> _states = new Dictionary<string, State>();
+
+        //This should allow serialization of the state
+        protected string currentState;
+
+        private State getState(string Key) {
+            if (_states.Count == 0 || string.IsNullOrEmpty(Key)) 
+                return null;
+            return _states[Key];
+        }
+
+        public string State {
+            get {
+                return currentState;
+            }
+            set {
+                if (currentState == value)
+                    return;
+
+                if (!string.IsNullOrEmpty(currentState) && !Transitions.Contains(value))
+                    throw new ArgumentException("Invalid Transition!");
+
+                State tmpState = getState(currentState);
+                if (null != tmpState && null != tmpState.OnExit) {
+                    foreach (State.EventHandler handler in getState(currentState).OnExit.GetInvocationList()) {
+                        handler(value);
+                    }
+                }
+
+                string prev_state = currentState;
+                currentState = value;
+
+                tmpState = getState(currentState);
+                if (null != tmpState && null != tmpState.OnEnter) {
+                    foreach (State.EventHandler handler in tmpState.OnEnter.GetInvocationList()) {
+                        handler(prev_state);
+                    }
+                }
+                    
+            }
+        }
+
+        public string[] Transitions { get { return getState(currentState).Transitions; } }
+
+        //Setup functions
+        protected State[] States {
+            set {
+                foreach(State s in value) {
+                    _states.Add(s.Key, s);
+                }
+            }
+        }
+
+        protected string InitialState {
+            set {
+                State = value;  //This makes sure onEnter gets called for the InitialState
+            }
+        }
+    }
+}
---
+++ TestMain.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using SimpleStateMachine;
+
+public class MachineState : StateMachine {
+
+    private void ComplexHandler(string value) {
+        Console.WriteLine("Left Paused State");
+    }
+
+    private void Initialize() {
+        States = new State[] {
+            new State("PAUSE", "Paused", new[] { "RUN", "STOP" }) {
+                OnEnter = (enter => { Console.WriteLine("Entered Paused State");}),
+                OnExit = ComplexHandler
+            },
+            new State("RUN", "Running", new[] { "PAUSE" }) {
+                OnEnter = (enter => { Console.WriteLine("Entered Running State"); }),
+                OnExit = (exit => { Console.WriteLine("Left Running State"); })
+            },
+            //A final state
+            new State("STOP", "Finished", null) {
+                OnEnter = (enter => { Console.WriteLine("Entered Final State"); })
+            }
+        };
+    }
+
+    public MachineState() {
+        Initialize();
+        InitialState = "PAUSE";  //This starts the workflow
+    }
+
+    public MachineState(string currentState) {
+        Initialize();
+        this.currentState = currentState;  //This continues a preexisting workflow
+    }
+
+}
+
+public class Test {
+
+    public static void Main(string[] args) {
+        MachineState csm = new MachineState();
+
+        csm.State = "RUN";
+        csm.State = "RUN";  //This should have no effect
+        try {
+            csm.State = "STOP";  //STOP isn't a valid transition
+        }
+        catch (ArgumentException) {
+            Console.WriteLine("Could not transition to invalid state!");
+        }
+        csm.State = "PAUSE";
+        csm.State = "STOP";
+
+        Console.WriteLine();
+        Console.WriteLine("Test continuation");
+        Console.WriteLine();
+
+        csm = new MachineState("RUN");
+        csm.State = "PAUSE";
+        csm.State = "STOP";
+
+        Console.ReadLine();
+    }
+
+    //Should Yield:
+    //Entered Paused State
+    //Left Paused State
+    //Entered Running State
+    //Could not transition to invalid state!
+    //Left Running State
+    //Entered Paused State
+    //Left Paused State
+    //Entered Final State
+    //
+    //Test continuation
+    //
+    //Left Running State
+    //Entered Paused State
+    //Left Paused State
+    //Entered Final State
+}
